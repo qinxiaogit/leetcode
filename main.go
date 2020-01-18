645acb96b5a3412844ea71579cc8e409b69d1c59
owlet <346243025@qq.com>
2020-01-18 17:59:46 +0800

    第四题

--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
 
 func main() {
 	//fmt.Println(t66y.LengthOfLongestSubstring("dvdf"))
-	fmt.Println(t66y.LengthOfLongestSubstring("cdd"))
+	//fmt.Println(t66y.LengthOfLongestSubstring("cdd"))
 	//fmt.Println(t66y.LengthOfLongestSubstring("abcabcbb"))
 	//fmt.Println(t66y.LengthOfLongestSubstring("bbbbb"))
 	//fmt.Println(t66y.LengthOfLongestSubstring("pwwkew"))
 	//fmt.Println(t66y.LengthOfLongestSubstring("bbbbb"))
+
+	//ret := t66y.FindMedianSortedArrays([]int{1,3},[]int{2})
+	ret := t66y.LongestPalindrome("babad")
+	fmt.Println(ret)
 }

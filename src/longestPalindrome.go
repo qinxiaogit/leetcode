798c13eb690eb7695e85aba2b438bfccd5f230c9
owlet <346243025@qq.com>
2020-01-19 17:01:38 +0800

    第五题

--- src/longestPalindrome.go
+++ src/longestPalindrome.go
@@ -5,18 +5,21 @@ func LongestPalindrome(s string) string {
 	maxStr := ""
 	for index, _ := range s {
 		mOdd := checkLongestPalindrome(s, index, index)    //奇数个
-		mEven := checkLongestPalindrome(s, index, index+1) //偶数个时
+		//mEven := checkLongestPalindrome(s, index, index+1) //偶数个时
 
-		oDdLength := mOdd*2 + 1
-		evenLength := mEven*2 + 2
+		oDdLength := mOdd*2 +1
+	//	evenLength := mEven*2
 		if oDdLength > maxLength {
-			maxStr = s[index-mOdd : index+mOdd]
+			maxStr = s[index-mOdd : index+mOdd+1]
 			maxLength = oDdLength
 		}
-		if evenLength > maxLength {
-			maxStr = s[index-mEven : index+mEven+1]
-			maxLength = evenLength
-		}
+		//if evenLength > maxLength {
+		//	maxStr = s[index-mEven : index+mEven+1]
+		//	maxLength = evenLength
+		//}
+
+
+
 	}
 	return maxStr
 }
@@ -26,7 +29,7 @@ func checkLongestPalindrome(s string, i, j int) int {
 	m := 0
 	for ; i-m > 0 && j+m < strLen; m++ {
 		if s[i-m] != s[i+m] {
-			break
+			return m-1
 		}
 	}
 	return m
--- src/longestPalindrome_test.go
+++ src/longestPalindrome_test.go
@@ -1,2 +1,15 @@
 package t66y
 
+import "testing"
+
+func TestLongestPalindrome(t *testing.T) {
+	if LongestPalindrome("aaaaaa") !="aaaaaa"{
+		t.Error("fail")
+	}
+}
+//"babad"
+func TestLongestPalindromeBab(t *testing.T) {
+	if LongestPalindrome("babad") !="bab"{
+		t.Error("fail")
+	}
+}
